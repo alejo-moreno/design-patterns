4d822c155d6039814bcb7bd75c04763380bd6832
Alejo Moreno <tecksonic@gmail.com>
2016-05-22 20:50:12 -0500

    initial commit

---
+++ BuildPattern.js
@@ -0,0 +1,51 @@
+
+class Director {
+
+    construct(builder) {
+        builder.step1();
+        builder.step2();
+        return builder.getResult();
+    }
+}
+
+class CarBuilder {
+    constructor() {
+        this.car = null;
+    }
+
+    step1() {
+        this.car = new Car();
+    }
+
+    step2() {
+        this.car.addParts();
+    }
+
+    getResult() {
+        return this.car;
+    }
+
+}
+
+class Car {
+    constructor() {
+        this.doors = 0;
+    }
+
+    addParts() {
+        this.doors = 4;
+    }
+
+    printDoors() {
+        console.log("Tengo " + this.doors + " puertas");
+    }
+}
+
+function testBuilderPattern() {
+    var shop = new Director();
+    var carBuilder = new CarBuilder();
+    var car = shop.construct(carBuilder);
+    car.printDoors();
+}
+
+testBuilderPattern();
